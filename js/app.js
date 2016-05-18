be74b79e343c68b517e48cc1d67b84be2b593df9
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-05-18 16:22:32 -0700

    fix indentation

--- js/app.js
+++ js/app.js
@@ -42,93 +42,93 @@ var productRank = { //Tracker Object Literal
   resultsButton: document.getElementById('showResults'),
   resetButton: document.getElementById('reset'),
 
-    getRandomIndex: function() { //from allProducts INCLUSIVE ZERO get number value to use in allProducts array
-      return Math.floor(Math.random() * productNames.length);
-    },
+  getRandomIndex: function() { //from allProducts INCLUSIVE ZERO get number value to use in allProducts array
+    return Math.floor(Math.random() * productNames.length);
+  },
 
-    displayImages: function() { //use getRandomIndex to display 3 imgs
-        //we need to assign an object to each of these things.
-        //run productRank.getRandomIndex(randomly gets an obj), get that value and use as index for product array and assign to leftObj
-        productRank.leftObj = allProducts[productRank.getRandomIndex()];
-        productRank.midObj = allProducts[productRank.getRandomIndex()];
-        productRank.rightObj = allProducts[productRank.getRandomIndex()];
-        //if any of the three obj images are the same, call again
-        if (productRank.leftObj === productRank.midObj || productRank.leftObj === productRank.rightObj || productRank.midObj === productRank.rightObj) {
-          productRank.displayImages(); //call itself again
-        }
-        //Now that no images are the same, use .src to update the path; use .id to update the name
-        productRank.leftEl.src = productRank.leftObj.path;
-        productRank.leftEl.id = productRank.leftObj.name;
-
-        productRank.midEl.src = productRank.midObj.path;
-        productRank.midEl.id = productRank.midObj.name;
-
-        productRank.rightEl.src = productRank.rightObj.path;
-        productRank.rightEl.id = productRank.rightObj.name;
-    },
-
-    tallyClicks: function(elId) { // when I click, add a vote to that img
-      // USE THE ID OF THE IMG WE CLICKED
-      for (var i in allProducts) {
-        if (elId === allProducts[i].name) {
-        allProducts[i].tally ++;
-        data.datasets[0].data[i] = allProducts[i].tally;// or +=1
-        this.totalClicks ++;
-
-				var storage = JSON.stringify(allProducts);
-					console.log(typeof(storage));
-
-				localStorage.setItem('votes', storage);
-
-				var storedVotes = localStorage.getItem('votes');
-				storedVotes = JSON.parse(storedVotes);
-					console.log(typeof(storedVotes));
+  displayImages: function() { //use getRandomIndex to display 3 imgs
+      //we need to assign an object to each of these things.
+      //run productRank.getRandomIndex(randomly gets an obj), get that value and use as index for product array and assign to leftObj
+      productRank.leftObj = allProducts[productRank.getRandomIndex()];
+      productRank.midObj = allProducts[productRank.getRandomIndex()];
+      productRank.rightObj = allProducts[productRank.getRandomIndex()];
+      //if any of the three obj images are the same, call again
+      if (productRank.leftObj === productRank.midObj || productRank.leftObj === productRank.rightObj || productRank.midObj === productRank.rightObj) {
+        productRank.displayImages(); //call itself again
       }
-      }
-    },
+      //Now that no images are the same, use .src to update the path; use .id to update the name
+      productRank.leftEl.src = productRank.leftObj.path;
+      productRank.leftEl.id = productRank.leftObj.name;
 
-    displayResults: function() {
+      productRank.midEl.src = productRank.midObj.path;
+      productRank.midEl.id = productRank.midObj.name;
 
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-					type: 'bar',
-					data: data,
-				});
-      },
+      productRank.rightEl.src = productRank.rightObj.path;
+      productRank.rightEl.id = productRank.rightObj.name;
+  },
 
-    showButton: function() { //at 15 votes, show button; click view Results; show RESET button
+  tallyClicks: function(elId) { // when I click, add a vote to that img
+    // USE THE ID OF THE IMG WE CLICKED
+    for (var i in allProducts) {
+      if (elId === allProducts[i].name) {
+      allProducts[i].tally ++;
+      data.datasets[0].data[i] = allProducts[i].tally;// or +=1
+				console.log(data.datasets[0].data[i]);
+      this.totalClicks ++;
 
-      this.resultsButton.hidden = false;
-      this.resultsButton.addEventListener('click', function() {
-        productRank.resetButton.hidden = false;
-        productRank.resultsButton.hidden = true;
-        productRank.displayResults();
+			var storage = JSON.stringify(allProducts);
+				console.log(typeof(storage));
 
-        productRank.resetButton.addEventListener('click', function() {
-        productRank.resetButton.hidden = true;
-        location.reload();
-        })
-      });
-    },
+			localStorage.setItem('votes', storage);
 
-    onClick: function() {
-      //IF you click on img's section, figure out which img
-      if (event.target.id === productRank.leftObj.name || event.target.id === productRank.midObj.name || event.target.id === productRank.rightObj.name) {
-          productRank.tallyClicks(event.target.id);
-
-          if (productRank.totalClicks % 15 === 0) {
-              //Put listener on section containing the images
-              productRank.imageEls.removeEventListener('click', productRank.onClick);
-              productRank.showButton();
-          }
-
-      productRank.displayImages();
-      } else {
-        alert('CLICK THE IMAGE');
-      }
-
+			var storedVotes = localStorage.getItem('votes');
+			storedVotes = JSON.parse(storedVotes);
+				console.log(typeof(storedVotes));
     }
-  };
+    }
+  },
+
+  displayResults: function() {
+
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var myChart = new Chart(ctx, {
+				type: 'bar',
+				data: data,
+			});
+    },
+
+  showButton: function() { //at 15 votes, show button; click view Results; show RESET button
+
+    this.resultsButton.hidden = false;
+    this.resultsButton.addEventListener('click', function() {
+      productRank.resetButton.hidden = false;
+      productRank.resultsButton.hidden = true;
+      productRank.displayResults();
+
+      productRank.resetButton.addEventListener('click', function() {
+      productRank.resetButton.hidden = true;
+      location.reload();
+      })
+    });
+  },
+
+  onClick: function() {
+    //IF you click on img's section, figure out which img
+    if (event.target.id === productRank.leftObj.name || event.target.id === productRank.midObj.name || event.target.id === productRank.rightObj.name) {
+        productRank.tallyClicks(event.target.id);
+
+        if (productRank.totalClicks % 15 === 0) {
+            //Put listener on section containing the images
+            productRank.imageEls.removeEventListener('click', productRank.onClick);
+            productRank.showButton();
+        }
+
+    productRank.displayImages();
+    } else {
+      alert('CLICK THE IMAGE');
+    }
+  }
+};
 //Listener is on section containing the images.
 productRank.imageEls.addEventListener('click', productRank.onClick);
 productRank.displayImages();//will give us first 3 images on page load.
